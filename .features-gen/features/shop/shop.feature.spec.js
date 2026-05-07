eda8e4fe32216608c44d5d2b4a9708e928d7b297
Chetanachar <chetanachar.k@gmail.com>
2026-05-07 17:27:31 +0530

    steps manually generated

--- .features-gen/features/shop/shop.feature.spec.js
+++ .features-gen/features/shop/shop.feature.spec.js
@@ -21,6 +21,11 @@ test.describe('Shop page functionality', () => {
     await Then('Cart badge should show "3"', null, { page }); 
   });
 
+  test('User can add multiple product to cart', { tag: ['@smoke'] }, async ({ When, Then, page }) => { 
+    await When('I add multiple items to cart and check cart count', {"dataTable":{"rows":[{"cells":[{"value":"iphone X"}]},{"cells":[{"value":"Samsung Note 8"}]}]}}, { page }); 
+    await Then('Cart badge should show "2"', null, { page }); 
+  });
+
 });
 
 // == technical section ==
@@ -40,4 +45,5 @@ const bddFileData = [ // bdd-data-start
   {"pwTestLine":10,"pickleLine":7,"tags":["@smoke"],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given I open shop page","isBg":true,"stepMatchArguments":[]},{"pwStepLine":11,"gherkinStepLine":8,"keywordType":"Outcome","textWithKeyword":"Then products should be visible","stepMatchArguments":[]}]},
   {"pwTestLine":14,"pickleLine":11,"tags":["@smoke"],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given I open shop page","isBg":true,"stepMatchArguments":[]},{"pwStepLine":15,"gherkinStepLine":12,"keywordType":"Action","textWithKeyword":"When I add \"iphone X\" to cart","stepMatchArguments":[{"group":{"start":6,"value":"\"iphone X\"","children":[{"start":7,"value":"iphone X","children":[{"children":[]}]},{"children":[{"children":[]}]}]},"parameterTypeName":"string"}]},{"pwStepLine":16,"gherkinStepLine":13,"keywordType":"Outcome","textWithKeyword":"Then Cart badge should show \"1\"","stepMatchArguments":[{"group":{"start":23,"value":"\"1\"","children":[{"start":24,"value":"1","children":[{"children":[]}]},{"children":[{"children":[]}]}]},"parameterTypeName":"string"}]}]},
   {"pwTestLine":19,"pickleLine":16,"tags":["@regression"],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given I open shop page","isBg":true,"stepMatchArguments":[]},{"pwStepLine":20,"gherkinStepLine":17,"keywordType":"Action","textWithKeyword":"When I add multiple items to cart and check cart count","stepMatchArguments":[]},{"pwStepLine":21,"gherkinStepLine":21,"keywordType":"Outcome","textWithKeyword":"Then Cart badge should show \"3\"","stepMatchArguments":[{"group":{"start":23,"value":"\"3\"","children":[{"start":24,"value":"3","children":[{"children":[]}]},{"children":[{"children":[]}]}]},"parameterTypeName":"string"}]}]},
+  {"pwTestLine":24,"pickleLine":24,"tags":["@smoke"],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given I open shop page","isBg":true,"stepMatchArguments":[]},{"pwStepLine":25,"gherkinStepLine":25,"keywordType":"Action","textWithKeyword":"When I add multiple items to cart and check cart count","stepMatchArguments":[]},{"pwStepLine":26,"gherkinStepLine":28,"keywordType":"Outcome","textWithKeyword":"Then Cart badge should show \"2\"","stepMatchArguments":[{"group":{"start":23,"value":"\"2\"","children":[{"start":24,"value":"2","children":[{"children":[]}]},{"children":[{"children":[]}]}]},"parameterTypeName":"string"}]}]},
 ]; // bdd-data-end
